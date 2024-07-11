781c3d30a57e8d512b1ccd4efc04fe9da74eaa13
luis-henrique-carvalho <luisdev9225@gmail.com>
2024-07-11 02:08:33 -0300

    Refactor action serializer for consistency and clarity

---
+++ app/serializers/action_serializer.rb
@@ -0,0 +1,28 @@
+# == Schema Information
+#
+# Table name: actions
+#
+#  id             :uuid             not null, primary key
+#  category       :integer          default("education"), not null
+#  description    :text
+#  end_date       :datetime         not null
+#  max_volunteers :integer          not null
+#  start_date     :datetime         not null
+#  title          :string           not null
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#  ong_id         :uuid             not null
+#
+# Indexes
+#
+#  index_actions_on_ong_id  (ong_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (ong_id => ongs.id)
+#
+class ActionSerializer
+  include JSONAPI::Serializer
+  attributes :title, :description, :start_date, :end_date,
+             :max_volunteers, :category, :created_at, :updated_at, :ong_id
+end
