71a8d23ec8725140b1c630d5e5c0d6fa79dcf6fe
luis-henrique-carvalho <luisdev9225@gmail.com>
2024-08-06 18:55:03 -0300

    Refactor UsersController and VolunteersController to improve JSON response consistency and readability

--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -1,10 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe 'Api::V1::Users', type: :request do
-  describe 'GET /show' do
-    it 'returns http success' do
-      get '/api/v1/users/show'
-      expect(response).to have_http_status(:success)
+  let!(:user) { create(:user) }
+  let!(:ong) { create(:ong, user:) }
+  let(:headers) { authenticated_header(user) }
+
+  describe 'GET /users:id' do
+    it 'returns the user' do
+      get(api_v1_user_path(user), headers: headers)
+
+      expect(response).to have_http_status(:ok)
+
+      json_response = JSON.parse(response.body, symbolize_names: true)
+      expect(json_response).to have_key(:data)
+      expect_user_attributes(json_response[:data])
     end
+  end
+
+  def expect_user_attributes(user)
+    expect(user).to have_key(:id)
+    expect(user).to have_key(:name)
+    expect(user).to have_key(:email)
+
+    expect(user[:ong]).to have_key(:id)
   end
 end
