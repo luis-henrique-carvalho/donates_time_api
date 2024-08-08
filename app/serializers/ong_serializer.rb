a375413cda64acd2a095f5634829d4f31aa29a86
luis-henrique-carvalho <luisdev9225@gmail.com>
2024-08-08 10:16:50 -0300

    Refactor OngSerializer to include total number of volunteers in the serialized output

--- app/serializers/ong_serializer.rb
+++ app/serializers/ong_serializer.rb
@@ -26,6 +26,10 @@ class OngSerializer < ApplicationSerializer
 
   fields :id, :category, :city, :description, :email, :name, :state, :user_id, :created_at, :updated_at
 
+  field :volunteers_total do |ong|
+    ong.volunteers.count
+  end
+
   view :with_user do
     association :user, blueprint: UserSerializer
   end
